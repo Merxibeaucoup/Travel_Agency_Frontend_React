e2823ebf84bb5631e4829a36843d3f43e30dde7d
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2022-11-05 13:31:44 -0400

    about step 1

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+
+import { useState } from "react";
 import { Helmet } from "react-helmet-async";
 
+import PageContainer from "../components/PageContainer";
+
 const About = () => {
+  const [state] = useState({
+    heading: "about us",
+    pageHeading: "world best travel agency company since 2005",
+    message:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, maiores optio. Amet, vitae. Est minima modi cumque pariatur voluptate ipsa, labore iste aliquam officiis tenetur nobis sunt quos sed rem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, maiores optio. Amet, vitae. Est minima modi cumque pariatur voluptate ipsa, labore iste aliquam officiis tenetur nobis sunt quos sed rem.",
+  });
   return (
     <>
       <Helmet>
         <title>About Page</title>
         <meta name="description" content="travel friends about page " />
       </Helmet>
-      <h1>About Page</h1>
+      <PageContainer data={state} />
     </>
   );
 };
